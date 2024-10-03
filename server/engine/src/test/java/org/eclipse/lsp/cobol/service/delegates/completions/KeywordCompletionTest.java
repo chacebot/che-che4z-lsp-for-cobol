5ec9169af278fabe41a38e08c48f18afe4f1fa2e
Chace Medeiros <chace.medeiros@broadcom.com>
2024-10-03 09:26:37 -0400

    update: add missing lexer token and further cleanup

--- server/engine/src/test/java/org/eclipse/lsp/cobol/service/delegates/completions/KeywordCompletionTest.java
+++ server/engine/src/test/java/org/eclipse/lsp/cobol/service/delegates/completions/KeywordCompletionTest.java
@@ -96,16 +96,16 @@ class KeywordCompletionTest {
 
     Keywords keywords = new Keywords(mock(SettingsService.class), dialectService);
     List<String> dialectType = ImmutableList.of();
-    assertEquals(2526, keywords.getDataMap(dialectType).size());
+    assertEquals(2525, keywords.getDataMap(dialectType).size());
 
     dialectType = ImmutableList.of("IDMS");
-    assertEquals(2527, keywords.getDataMap(dialectType).size());
+    assertEquals(2526, keywords.getDataMap(dialectType).size());
 
     dialectType = ImmutableList.of("DaCo");
-    assertEquals(2528, keywords.getDataMap(dialectType).size());
+    assertEquals(2527, keywords.getDataMap(dialectType).size());
 
     dialectType = ImmutableList.of("DaCo", "IDMS");
-    assertEquals(2529, keywords.getDataMap(dialectType).size());
+    assertEquals(2528, keywords.getDataMap(dialectType).size());
   }
 
   @Test
